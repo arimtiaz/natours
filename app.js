3d475de522d83f5bac298eb2fe85e8a3ae0565f7
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-07 16:10:59 +0600

    Working

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+app.use(express.json())
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results : tours,
+    data:{
+        tours
+    }
+  });
+});
+
+app.post('/api/v1/tours', (req, res)=>{
+
+    const newId = tours[tours.length-1].id+1;
+    // const newId = Math.random(100);
+    const newTour = Object.assign({id: newId}, req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err=>{
+        res.status(201).json({
+            status:'success',
+            data:{
+                tours: newTour
+            }
+        })
+    })
+})
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}`)
+})
