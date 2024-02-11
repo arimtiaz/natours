9ea0a1c9af912a40b247fc6ff03d5742619937c0
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-11 13:07:31 +0600

    Mongo Connected

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = require('./app')
+dotenv.config({path: './config.env'})
+const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
+console.log(DB)
 
 const port = 3000;
 app.listen(port, () => {
