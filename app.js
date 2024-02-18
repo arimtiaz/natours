09d57dd9a121cd041b7523764fad015877bef1a6
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2024-02-18 11:25:54 +0600

    Mongo Connected

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/userRouter');
 const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
--- server.js
+++ server.js
@@ -3,8 +3,15 @@ const dotenv = require('dotenv')
 const app = require('./app')
 dotenv.config({path: './config.env'})
 const DB = process.env.DATABASE.replace('<password>', process.env.PASSWORD);
-console.log(DB)
-
+// console.log(DB)
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex : true,
+  useFindAndModify: false
+}).then(con => {
+  // console.log(con.connections);
+  console.log('DB Connections Successful')
+})
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
